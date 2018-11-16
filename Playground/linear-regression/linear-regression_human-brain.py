466ccaf93d44bd93f1a42f347cad0b524984ebe1
Marvin Sengera <work@marvin-sengera.de>
2018-11-16 18:17:42 +0100

    Plot regression data

--- Playground/linear-regression/linear-regression_human-brain.py
+++ Playground/linear-regression/linear-regression_human-brain.py
@@ -34,5 +34,23 @@ b0 = y_mean - (b1 * x_mean)
 # printing the coefficient
 print(b1, b0)
 
+# plotting values
+x_max = np.max(X) + 100
+x_min = np.min(X) - 100
 
+# calculating line values of x and y
+x = np.linspace(x_min, x_max, 1000)
+y = b0 + b1 * x
+
+# plotting line
+plt.plot(x, y, color='#00ff00', label='Linear Regression')
+
+# plot the data point
+plt.scatter(X, Y, color='#ff0000', label='Data Point')
+
+plt.xlabel('Head Size (cm^3)')
+plt.ylabel('Brain Weight (grams)')
+
+plt.legend()
+plt.show()
 
